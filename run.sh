ba24a9585afc47184d4b2f1a1ecd3bb788c39601
Bryan-Az <36939025+Bryan-Az@users.noreply.github.com>
2023-11-14 17:00:50 -0800

    tested metadata processing script in nb

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 # create conda environment (must have installed conda) env name: audio-analysis
 conda env create -f environment.yml
+python -m ipykernel install --user --name audio-analysis --display-name "audio-analysis"
